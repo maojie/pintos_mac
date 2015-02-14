62d471b56648b8985b05bb1b496d6f9cd8da1432
Daniel Gibbs <daniel@danielgibbs.name>
2015-02-14 23:12:51 +1300

    Remove stropts.h include from utils and comment out code that used it

--- utils/squish-pty.c
+++ utils/squish-pty.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stropts.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -288,13 +287,15 @@ main (int argc __attribute__ ((unused)), char *argv[])
     fail_io ("open \"%s\"", name);
 
   /* System V implementations need STREAMS configuration for the
-     slave. */
+     slave. This is commented out as OS X doesn't provide stropts.h.
+
   if (isastream (slave))
     {
       if (ioctl (slave, I_PUSH, "ptem") < 0
           || ioctl (slave, I_PUSH, "ldterm") < 0)
         fail_io ("ioctl");
     }
+  */
 
   /* Arrange to get notified when a child dies, by writing a byte
      to a pipe fd.  We really want to use pselect() and
--- utils/squish-unix.c
+++ utils/squish-unix.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stropts.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/time.h>
